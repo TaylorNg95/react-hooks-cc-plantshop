292459aa091fa6fe0805f989478ae7f880052c42
Taylor Ng <taylormng95@gmail.com>
2024-06-11 09:54:23 -0400

    completes code challenge

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,37 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([])
+  const [search, setSearch] = useState('')
+
+  useEffect(() => {
+    fetch('http://localhost:6001/plants')
+      .then(response => response.json())
+      .then(plants => setPlants(plants))
+  }, [])
+
+  const displayedPlants = plants.filter(plant => plant.name.toLowerCase().includes(search))
+
+  function addNewPlant(newPlant){
+    fetch('http://localhost:6001/plants', {
+      method: 'POST',
+      headers: {
+        "Content-Type": "Application/JSON",
+      },
+      body: JSON.stringify(newPlant)
+    })
+      .then(response => response.json())
+      .then(newPlant => setPlants([...plants, newPlant]))
+  }
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm addNewPlant={addNewPlant}/>
+      <Search search={search} setSearch={setSearch}/>
+      <PlantList plants={displayedPlants}/>
     </main>
   );
 }
